3b451e024bf08d214e8daf01a6ff2cf175f8cc14
r0lfs <rdwoolen@gmail.com>
2017-11-29 16:20:34 -0700

    added gem 'paranoia' which adds soft delete capabilities

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,10 @@
 class Answer < ApplicationRecord
 	belongs_to :user
+
+	def user
+  	User.unscoped { super }
+	end
+	
 	belongs_to :question
   has_many :comments, as: :commentable
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
 	belongs_to :user
+
+	def user
+  	User.unscoped { super }
+	end
+	
   has_many :comments, as: :commentable
 
 end
