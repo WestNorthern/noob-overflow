4d0053b52c04e481b541adbb3236e1fe9b8b5aef
r0lfs <rdwoolen@gmail.com>
2017-11-29 17:20:18 -0700

    minor changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
   belongs_to :user
+
+  def user
+  	User.unscoped { super }
+	end
   
 end
