a1f18574f2e0cbecf8885e445580fc43604e26e6
r0lfs <rdwoolen@gmail.com>
2017-12-03 11:24:44 -0700

    results sorted by rep on search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
   	@search = params[:search].split()
-  	@results = Answer.tagged_with(@search, :any => true)
+  	@results = Answer.tagged_with(@search, :any => true).order(reputation: :desc)
   end
 end
