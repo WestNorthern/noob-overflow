deaa2cb28135e45a182b530f50e3e4bb2dfa8cdb
r0lfs <rdwoolen@gmail.com>
2017-12-03 13:26:03 -0700

    sorted by rep on search, plus displays matching tags

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
   def index
   	@search = params[:search].split()
-  	@results = Answer.tagged_with(@search, :any => true).order(reputation: :desc)
+  	results = []
+  	answers = Answer.tagged_with(@search, :any => true)
+  	questions = Question.tagged_with(@search, :any => true)
+  	#@post = Post.tagged_with(@search, :any => true).order(reputation: :desc)
+  	results += answers + questions #+ @posts
+  	results.sort! {|a, b| a.reputation <=> b.reputation}
+  	@results = results.reverse
   end
 end
